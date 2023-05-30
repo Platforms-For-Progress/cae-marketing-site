ca1e4bffd394024a6a43674be954bdb7e3e1d1a9
Jacob Shalabi <jshalabi03@gmail.com>
2023-05-30 01:01:04 -0500

    tried to set default color mode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,27 @@
 "use client";
 
+import { ColorModeScript } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  extendTheme,
+  type ThemeConfig,
+} from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
+    <>
+      <ColorModeScript initialColorMode="light" />
+      <CacheProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </CacheProvider>
+    </>
   );
 }
